58542dfac042aaa17b778e5fb282c92cc25817da
melissalee <melissa.f.lee02@gmail.com>
2019-03-09 17:44:37 -0800

    parsed Education, Employment, and Election Data (contains bugs)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        String educationData = Utils.readFileAsString("data/Education.csv");
+        String employmentData = Utils.readFileAsString("data/Unemployment.csv");
         ArrayList<ElectionResult> results = Utils.parse2016PresidentialResults(data);
-        for (ElectionResult result : results) {
-            System.out.println(result.toString());
+        DataManager dataManager = Utils.parseEducationEmploymentElectionData(educationData, employmentData, data);
+        List<State> states = dataManager.getStates();
+        for (State state : states) {
+            List<County> counties = state.getCounties();
+            for (County county : counties) {
+                System.out.println("State: " + state.getName() + "\n"
+                + "County: " + county.getName() + "\n"
+                + "Fips: " + county.getFips());
+            }
         }
     }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -59,34 +59,85 @@ public class Utils {
         return input;
     }
 
-    public static DataManager parseEducationData(String data) {
-        String[] lines = data.split("\n");
+    public static DataManager parseEducationEmploymentElectionData(String educationData, String employmentData, String electionData) {
+        DataManager dataManager = new DataManager();
+        String[] educationLines = educationData.split("\n");
+        createStates(educationLines, dataManager);
+        addCountiesToState(educationLines, dataManager, employmentData, electionData);
+
+        // percent of adults with less than a high school diploma: index 11
+
+        return dataManager;
+    }
+
+    public static void addCountiesToState(String[] lines, DataManager dataManager, String employmentData, String electionData) {
+        for (int i = 6; i < 3288; i++){
+            String currentLine = removeExtraSpacesAndCommasInQuotes(lines[i]);
+            String[] components = currentLine.split(",");
+            State currentState = dataManager.getState(components[1]);
+            County currentCounty =  new County(components[2], Integer.parseInt(components[0]));
+            Education2016 education2016 = new Education2016(Double.parseDouble(components[43]), Double.parseDouble(components[44]),
+                    Double.parseDouble(components[45]), Double.parseDouble(components[46]));
+            currentCounty.setEduc2016(education2016);
+            Employment2016 employment2016 = getEmployment2016(employmentData, i + 3);
+            currentCounty.setEmploy2016(employment2016);
+            Election2016 election2016 = getElection2016(electionData, currentCounty.getName());
+            currentCounty.setVote2016(election2016);
+            currentState.addCounty(currentCounty);
+        }
+    }
+
+    public static Election2016 getElection2016(String electionData, String countyName) {
+        ArrayList<ElectionResult> electionResults = parse2016PresidentialResults(electionData);
+        ElectionResult currentElectionResult = getCurrentElectionResult(electionResults, countyName);
+        Election2016 election2016 = new Election2016(currentElectionResult.getVotes_dem(), currentElectionResult.getVotes_gop(), currentElectionResult.getTotal_votes());
+        return election2016;
+    }
+
+    public static ElectionResult getCurrentElectionResult(ArrayList<ElectionResult> electionResults, String countyName) {
+        for (ElectionResult electionResult : electionResults) {
+            if (electionResult.getCounty_name().equals(countyName)) {
+                return electionResult;
+            }
+        }
         return null;
     }
 
-    public static String removeCommasInQuotes(String input) {
+    public static Employment2016 getEmployment2016(String employmentData, int index) {
+        String[] employmentLines = employmentData.split("\n");
+        String currentLine = removeExtraSpacesAndCommasInQuotes(employmentLines[index]);
+        String[] components = currentLine.split(",");
+        System.out.println("Employment line: " + currentLine);
+        Employment2016 employment2016 = new Employment2016(Integer.parseInt(components[42].substring(1, components[42].length()-1)), Integer.parseInt(components[43].substring(1, components[43].length()-1)), Integer.parseInt(components[44].substring(1, components[44].length()-1)), Double.parseDouble(components[45]));
+        return employment2016;
+    }
+
+    public static void createStates(String[] lines, DataManager dataManager) {
+        for (int i = 6; i < 3288; i++) {
+            String currentLine = removeExtraSpacesAndCommasInQuotes(lines[i]);
+            String[] components = currentLine.split(",");
+            System.out.println(components[1] + ", " + i);
+            if (!dataManager.containsState(components[1])) {
+                State newState = new State(components[1]);
+                dataManager.addState(newState);
+            }
+        }
+    }
+
+    public static String removeExtraSpacesAndCommasInQuotes(String input) {
         boolean inQuotes = false;
         for (int i = 0; i < input.length(); i++) {
             if (input.substring(i, i+1).equals("\"")){
                 inQuotes = !inQuotes;
             }
-            if (inQuotes && input.substring(i, i+1).equals(",")) {
+            if (inQuotes && input.substring(i, i+1).equals(",") || inQuotes && input.substring(i, i+1).equals("\t")
+                    || inQuotes && input.substring(i, i+1).equals(" ")) {
                 input = input.substring(0, i) + input.substring(i+1);
+                i--;
             }
+
         }
         return input;
     }
 
-    public static String removeConsecutiveCommas(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            if (input.substring(i, i+1).equals(",")) {
-                int j = i+1;
-                while (j < input.length() && input.substring(j, j+1).equals(",")) {
-                    j++;
-                }
-                input = input.substring(0, i+1) + input.substring(j);
-            }
-        }
-        return input;
-    }
 }
